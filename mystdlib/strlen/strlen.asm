59c9bb20d1d3214a252f47d00dac3fe1a11cc782
ROPChain <73626082+Jorge-Felix@users.noreply.github.com>
2026-04-20 19:50:59 -0400

    Add files via upload

---
+++ mystdlib/strlen/strlen.asm
@@ -0,0 +1,13 @@
+default rel
+
+section .text
+global strlen
+strlen:
+    xor rax, rax        ; zero rax
+.loop:
+    cmp byte [rcx + rax], 0
+    je .done
+    inc rax
+    jmp .loop
+.done:
+    ret
